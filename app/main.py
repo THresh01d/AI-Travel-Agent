3eee43023b5caa8259208c3e27eedcdb384fcb95
Chun Ye <1961420660@qq.com>
2026-05-28 19:20:52 +0800

    connect deepseek api safely

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
 
 app = FastAPI()
+messages = []
 
 class ChatRequest(BaseModel):
     message: str
+
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 
 @app.get("/")
 def home():
@@ -12,7 +20,45 @@ def home():
 
 @app.post("/chat")
 def chat(req: ChatRequest):
+
+    url = "https://api.deepseek.com/chat/completions"
+
+    headers = {
+        "Authorization": f"Bearer {DEEPSEEK_API_KEY}",
+        "Content-Type": "application/json"
+    }
+
+    messages.append({
+        "role": "user",
+        "content": req.message
+    })
+
+    full_messages =[
+        {
+            "role":"system",
+            "content":"你是一个专业的旅游规划师,擅长生成旅行攻略"
+        }
+    ] +messages
+
+    data = {
+        "model": "deepseek-chat",
+        "messages": full_messages
+    }
+
+    response = requests.post(url, headers=headers, json=data)
+
+    result = response.json()
+
+    ai_reply = result["choices"][0]["message"]["content"]
+
+    messages.append({
+        "role": "assistant",
+        "content": ai_reply
+    })
+
     return {
         "user_message": req.message,
-        "ai_response": f"你刚刚说了: {req.message}"
-    }    
+        "ai_response": ai_reply
+    }
+
+    
